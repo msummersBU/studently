74d0f0b14f98cf15fdf503f3808a5f5a6a6b0acb
Michael Summers <s5206096@bournemouth.ac.uk>
2020-11-02 10:29:49 +0000

    First commit

---
+++ public/navbar.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <div id="navigation">
+       <ul>
+          <h1>
+            <li><a href="index.html">Decks</a></li>
+            <li><a href="create.html">Create Card</a></li>
+            <li><a href="settings.html">Settings</a></li>
+            <li><a href="statistics.html">Statistics</a></li>
+            <li id="logininfo">Logged in as: Students</li>
+          </h1>
+        </ul>
+    </div>
+</html>
